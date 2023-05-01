3bab090325649835cb82e756c82ea937b4328831
Sol <a22solmi@student.his.se>
2023-05-01 13:05:41 +0200

    Added RecyclerItem, representing one item in the RecyclerView

---
+++ app/src/main/java/com/example/project/RecyclerItem.java
@@ -0,0 +1,12 @@
+package com.example.project;
+
+public class RecyclerItem {
+    private String name;
+
+    public RecyclerItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() { return name; }
+
+}
