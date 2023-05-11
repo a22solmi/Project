f8fbc4237ba0a336b95efa92d5eb984d22348af5
Sol <a22solmi@student.his.se>
2023-05-11 08:46:07 +0200

    Updated recyclerItem to use new json data

--- app/src/main/java/com/example/project/RecyclerItem.java
+++ app/src/main/java/com/example/project/RecyclerItem.java
@@ -1,12 +1,31 @@
 package com.example.project;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
 public class RecyclerItem {
     private String name;
+    @SerializedName("company")
+    private String use;
 
-    public RecyclerItem(String name) {
-        this.name = name;
-    }
+    @SerializedName("location")
+    private String paradigm;
+    @SerializedName("category")
+    private String example;
+    @SerializedName("size")
+    private int collector;
+
+    @SerializedName("auxdata")
+    private AuxData aux;
 
     public String getName() { return name; }
+
+    public String getParadigm() { return paradigm; }
+    public String getUse() { return use; }
+    public String getExample() { return example; }
+    public int getCollector() { return collector; }
+    public String getImg() { return aux.getImg(); }
 
 }
