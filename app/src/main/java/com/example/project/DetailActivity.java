3fae5e8f32a9fbb7364fdde039338d3d1f18e43d
Sol <a22solmi@student.his.se>
2023-05-12 09:38:24 +0200

    Added fields to detail activity and added Picasso library for loading pictures

--- app/src/main/java/com/example/project/DetailActivity.java
+++ app/src/main/java/com/example/project/DetailActivity.java
@@ -3,6 +3,11 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        getSupportActionBar().setTitle(getIntent().getStringExtra("name"));
+
+        ImageView imageView = findViewById(R.id.detail_image);
+        Picasso.get()
+                .load("file:///android_asset/" + getIntent().getStringExtra("img"))
+                .resize(300, 300)
+                .into(imageView);
+
+        TextView detailParadigm = findViewById(R.id.detail_paradigm);
+        detailParadigm.setText("Paradigm: ");
+        LinearLayout linearLayout = findViewById(R.id.paradigm_layout);
+        String[] paradigms = getIntent().getStringArrayExtra("paradigm");
+        for (String paradigm: paradigms) {
+            TextView textView = new TextView(this);
+            textView.setText(paradigm);
+            textView.setBackgroundResource(R.drawable.tag_background);
+            textView.setPadding(10, 0, 10, 0);
+            linearLayout.addView(textView);
+        }
+
+
+        TextView detailUse = findViewById(R.id.detail_use);
+        detailUse.setText(getIntent().getStringExtra("use"));
+
+        TextView detailExample = findViewById(R.id.detail_example);
+        detailExample.setText(getIntent().getStringExtra("example"));
     }
 }
