074733ec331d479a383ad0d8c1c186d7b52b4fbf
Sol <a22solmi@student.his.se>
2023-05-22 15:42:00 +0200

    Added image and text to about activity

--- app/src/main/java/com/example/project/AboutActivity.java
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -3,6 +3,9 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -11,5 +14,10 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         getSupportActionBar().setTitle("About");
+
+        ImageView imageView = findViewById(R.id.detail_image);
+        Picasso.get()
+                .load("file:///android_asset/programming.png")
+                .into(imageView);
     }
 }
